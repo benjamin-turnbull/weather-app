aafd1f1ef7373ce54cafbc7dc4bc9696a5733d99
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-01-02 12:28:44 +1030

    implemented API and provider

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Home from "./Pages/Home";
-
+import {ApolloClient, InMemoryCache, ApolloProvider, } from '@apollo/client';
 
 function App() {
-  return <div className="App">
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https://graphql-weather-api.herokuapp.com/"
+  })
+
+  return (
+  <ApolloProvider client={client}>
     <Home />
-  </div>;
+  </ApolloProvider>
+  )
 }
 
 export default App;
