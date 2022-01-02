db236939f4ded3280ec63b847e07d9632888a907
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-01-02 12:17:55 +1030

    created home page elements

---
+++ weather-app/src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+
+function Home() {
+    return <div className="home">
+        <h1>Search For Weather</h1>
+        <input type="text" placeholder="city name"></input>
+        <button>Search</button>
+    </div>
+}
+
+export default Home;
