3ed467592ee54c888453e58416ccb3a3ae519863
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-01-02 20:01:24 +1030

    added video background

--- docs/src/Pages/Home.js
+++ docs/src/Pages/Home.js
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { GET_WEATHER_QUERY } from "../graphql/Queries";
 import "../App.css";
+import globe from './videos/video-2.mp4';
 
 function Home() {
   const [citySearched, setCitySearched] = useState("");
   const [getWeather, { data, error }] = useLazyQuery(GET_WEATHER_QUERY, {
     variables: { name: citySearched },
   });
+
+
 
   if (error) return <h1> Error found</h1>;
 
@@ -17,8 +20,10 @@ function Home() {
 
   return (
     <div className="hero-container">
-    <video src='../../public/videos/video-2.mp4' autoPlay loop muted />
-      <h1 className="header">Search For Weather</h1>
+    <video autoPlay loop muted>
+        <source src={globe} type="video/mp4"/>
+    </video>
+      {/* <h1 className="header">Search For Weather</h1> */}
       <input
         className="field"
         type="text"
@@ -28,7 +33,7 @@ function Home() {
         }}
       />
       <button className="label" onClick={() => getWeather()}> Search</button>
-      <div>
+      <div className="text">
         {data && (
           <>
             <h1> {data.getCityByName.name} </h1>
