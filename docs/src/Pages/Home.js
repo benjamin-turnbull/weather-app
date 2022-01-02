3f920b57bad496ca070872e2fa0acd34cf607d35
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-01-02 12:44:29 +1030

    created graphql queries

--- docs/src/Pages/Home.js
+++ docs/src/Pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react"; 
 
 function Home() {
-    return <div className="home">
+    return (<div className="home">
         <h1>Search For Weather</h1>
         <input type="text" placeholder="city name"></input>
         <button>Search</button>
     </div>
+    );
 }
 
 export default Home;
